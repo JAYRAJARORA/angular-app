583b17cfde7f75a92af3f911deeec31190939b1b
Jayraj Arora <jayraj.arora@gmail.com>
2024-02-18 08:26:09 -0800

    Create two basic components one manually and another using CLI

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styles: [`
+  p {
+    background-color: palegreen;
+    padding: 20px;
+    border: 1px solid  darkgreen;
+  }
+  `] 
+})
+export class SuccessAlertComponent {
+
+}
